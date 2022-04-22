eebf938e697ac116a061a50c0c8a7a89491c1b38
Alexander Chamberlain <alexander.chamberlain@softwire.com>
2022-04-22 12:31:38 +0100

    fetch stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [randomDrink, setRandomDrink] = useState('random drink')
+
+  useEffect(() => {
+    changeDrink()
+  }, []);
+  
+  function changeDrink() {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+    .then(res => res.json())
+    .then(result => setRandomDrink(result.drinks[0].strDrink))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{randomDrink}</h1>
+        <button onClick={() => changeDrink()}>Change</button>
       </header>
     </div>
   );
