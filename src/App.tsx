43b75e8de4fda6122200a104158e720fbbcc6e47
Emilie <emilie.stone@softwire.com>
2022-04-29 11:24:39 +0100

    Make cocktail model

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import {Cocktail} from "./models";
 import './App.css';
 
 function App() {
-  const [randomDrinkName, setRandomDrinkName] = useState('random drink')
-  const [randomDrinkImage, setRandomDrinkImage] = useState('random drink')
+  const [randomCocktail, setRandomCocktail] = useState<Cocktail | undefined>(undefined);
+  const [randomCocktailList, setCocktailList] = useState<Cocktail[]>();
 
-
+// 20 cocktails in random order
   useEffect(() => {
     changeDrink()
   }, []);
   
+  function getAndStoreRandomDrinks(noOfDrinks: number) {
+    for (let i=0; i<noOfDrinks; i++) {
+      
+    }
+  }
+
+  function fetchNewDrink() {
+    return (
+      fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+      .then(res => res.json()))
+  }
+
   function changeDrink() {
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
-    .then(res => res.json())
+    fetchNewDrink()
     .then(result => {
-      setRandomDrinkName(result.drinks[0].strDrink)
-      setRandomDrinkImage(result.drinks[0].strDrinkThumb)
+      const newCocktail = {
+        name: result.drinks[0].strDrink,
+        imageUrl: result.drinks[0].strDrinkThumb
+      };
+      setRandomCocktail(newCocktail);
     })
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{randomDrinkName}</h1>
-        <img src={randomDrinkImage}></img>
+        <h1>{randomCocktail?.name}</h1>
+        <img src={randomCocktail?.imageUrl}></img>
         <button onClick={() => changeDrink()}>Change</button>
       </header>
     </div>
