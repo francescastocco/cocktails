f35363dbd0a90831abeaee9c76b0eb0fe6f61024
Alexander Chamberlain <alexander.chamberlain@softwire.com>
2022-04-29 12:12:17 +0100

    card

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Card } from './components/Card';
+
+interface DrinkResponse {
+  strDrink: string
+  strDrinkThumb: string
+}
 
 function App() {
-  const [randomDrink, setRandomDrink] = useState('random drink')
+  const [randomDrink, setRandomDrink] = useState<DrinkResponse>()
 
   useEffect(() => {
     changeDrink()
@@ -12,13 +17,19 @@ function App() {
   function changeDrink() {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
     .then(res => res.json())
-    .then(result => setRandomDrink(result.drinks[0].strDrink))
+    .then(result => setRandomDrink(result.drinks[0]))
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{randomDrink}</h1>
+        <div style={{display: 'flex'}}>
+          {[randomDrink, randomDrink, randomDrink, randomDrink, randomDrink, randomDrink].map((drink: DrinkResponse | undefined, index: number) => {
+            return <Card id={index} onClick={(id: number) => console.log(`id ${id} clicked`)} image={randomDrink?.strDrinkThumb} />
+          })}
+        </div>
+        <h1>{randomDrink?.strDrink}</h1>
+        <img src={randomDrink?.strDrinkThumb} alt={randomDrink?.strDrink} />
         <button onClick={() => changeDrink()}>Change</button>
       </header>
     </div>
