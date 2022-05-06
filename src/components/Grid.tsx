33d426dd76e9dcd75f092bf8bad06ececceea6dd
Alexander Chamberlain <alexander.chamberlain@softwire.com>
2022-05-06 12:25:15 +0100

    Fix

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,11 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import internal from 'stream';
+import { useState } from 'react';
 import { Cocktail } from '../models';
 import '../styles/Grid.css';
 import { Card } from './Card';
-
-interface GridProps {
-}
 
 const cocktail1 = {name: 'Orange Crush', imageUrl: 'https://www.thecocktaildb.com/images/media/drink/svsvqv1473344558.jpg'}
 const cocktail2 = {name: 'Pain', imageUrl: 'https://www.thecocktaildb.com/images/media/drink/3nbu4a1487603196.jpg'}
@@ -18,22 +14,15 @@ const cocktail8 = {name: 'phjhvhon', imageUrl: 'https://www.thecocktaildb.com/im
 const cocktail9 = {name: 'pihhhhhhoon', imageUrl: 'https://www.thecocktaildb.com/images/media/drink/of1rj41504348346.jpg'}
 const cocktail10 = {name: 'pioi32uymoon', imageUrl: 'https://www.thecocktaildb.com/images/media/drink/bcsj2e1487603625.jpg'}
 
-
-
-export function Grid({}: GridProps) {
-    const [drinksList, setDrinksList] = useState<number[]>([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
+export function Grid() {
     const [selectedDrinks, setSelectedDrinks] = useState<number[]>([]);
-    const [selectedCardOneName, setselectedCardOneName] = useState<string | undefined>(undefined);
     const [foundCardIndexes, setFoundCardIndexes] = useState<number[]>([]);
+
     const randomCocktailList = [cocktail1, cocktail1, cocktail2, cocktail3, cocktail5, 
       cocktail8, cocktail7, cocktail8, cocktail9, cocktail10,
       cocktail4, cocktail2, cocktail7, cocktail4, cocktail6,
       cocktail6, cocktail5, cocktail9, cocktail3, cocktail10];
     
-
-      
-
-
       const onCardPressed = (id: number, cocktailName: string) => {
         console.log(`id ${id} clicked`)
         if (foundCardIndexes?.includes(id)) {
@@ -42,17 +31,18 @@ export function Grid({}: GridProps) {
         if (selectedDrinks.length === 2) {
           return
         }
-        setSelectedDrinks([...selectedDrinks, id])
-        if (selectedDrinks.length === 2) {
-          var card1name = randomCocktailList[selectedDrinks[0]].name;
-          var card2name = randomCocktailList[selectedDrinks[1]].name;
+        const drinks = [...selectedDrinks, id]
+        setSelectedDrinks(drinks)
+        if (drinks.length === 2) {
+          var card1name = randomCocktailList[drinks[0]].name;
+          var card2name = randomCocktailList[drinks[1]].name;
 
           if (card1name === card2name) {
-            setFoundCardIndexes([...foundCardIndexes, ...selectedDrinks])
+            setFoundCardIndexes([...foundCardIndexes, ...drinks])
             setSelectedDrinks([])
-          }
-          else {
-            setTimeout(() => {setSelectedDrinks([])}, 2000)
+            alert('Congrats you found a pair!')
+          } else {
+            setTimeout(() => {setSelectedDrinks([])}, 1500)
           }
           return
         }
@@ -60,10 +50,7 @@ export function Grid({}: GridProps) {
     };
     
     return <div className='grid-container'>
-        {[cocktail1, cocktail1, cocktail2, cocktail3, cocktail5, 
-          cocktail8, cocktail7, cocktail8, cocktail9, cocktail10,
-          cocktail4, cocktail2, cocktail7, cocktail4, cocktail6,
-          cocktail6, cocktail5, cocktail9, cocktail3, cocktail10].map((drink: Cocktail, index: number) => {
+        {randomCocktailList.map((drink: Cocktail, index: number) => {
             return <div className='grid-item'>
                 <Card id={index} onClick={onCardPressed} flipped={foundCardIndexes.includes(index)||selectedDrinks.includes(index)} image={drink.imageUrl} name={drink.name}/>
                 </div>
